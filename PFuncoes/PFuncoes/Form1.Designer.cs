5940b91fc9881bd8982f2f743c3d0dd59839aeb4
NICOLAS LEME QUEVEDO SALLES <0030482421009@lti.fatecso.edu.br>
2025-04-29 09:04:29 -0300

    aula 29/04

---
+++ PFuncoes/PFuncoes/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace PFuncoes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSoma1 = new System.Windows.Forms.Button();
+            this.btnSoma2 = new System.Windows.Forms.Button();
+            this.btnSoma3 = new System.Windows.Forms.Button();
+            this.btnSoma4 = new System.Windows.Forms.Button();
+            this.btnSoma5 = new System.Windows.Forms.Button();
+            this.btnSoma6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSoma1
+            // 
+            this.btnSoma1.Location = new System.Drawing.Point(317, 191);
+            this.btnSoma1.Name = "btnSoma1";
+            this.btnSoma1.Size = new System.Drawing.Size(75, 23);
+            this.btnSoma1.TabIndex = 0;
+            this.btnSoma1.Text = "Soma1";
+            this.btnSoma1.UseVisualStyleBackColor = true;
+            this.btnSoma1.Click += new System.EventHandler(this.btnSoma1_Click);
+            // 
+            // btnSoma2
+            // 
+            this.btnSoma2.Location = new System.Drawing.Point(413, 191);
+            this.btnSoma2.Name = "btnSoma2";
+            this.btnSoma2.Size = new System.Drawing.Size(75, 23);
+            this.btnSoma2.TabIndex = 1;
+            this.btnSoma2.Text = "Soma2";
+            this.btnSoma2.UseVisualStyleBackColor = true;
+            this.btnSoma2.Click += new System.EventHandler(this.btnSoma2_Click);
+            // 
+            // btnSoma3
+            // 
+            this.btnSoma3.Location = new System.Drawing.Point(306, 220);
+            this.btnSoma3.Name = "btnSoma3";
+            this.btnSoma3.Size = new System.Drawing.Size(75, 23);
+            this.btnSoma3.TabIndex = 2;
+            this.btnSoma3.Text = "Soma3";
+            this.btnSoma3.UseVisualStyleBackColor = true;
+            this.btnSoma3.Click += new System.EventHandler(this.btnSoma3_Click);
+            // 
+            // btnSoma4
+            // 
+            this.btnSoma4.Location = new System.Drawing.Point(413, 220);
+            this.btnSoma4.Name = "btnSoma4";
+            this.btnSoma4.Size = new System.Drawing.Size(75, 23);
+            this.btnSoma4.TabIndex = 3;
+            this.btnSoma4.Text = "Soma4";
+            this.btnSoma4.UseVisualStyleBackColor = true;
+            this.btnSoma4.Click += new System.EventHandler(this.btnSoma4_Click);
+            // 
+            // btnSoma5
+            // 
+            this.btnSoma5.Location = new System.Drawing.Point(306, 249);
+            this.btnSoma5.Name = "btnSoma5";
+            this.btnSoma5.Size = new System.Drawing.Size(75, 23);
+            this.btnSoma5.TabIndex = 4;
+            this.btnSoma5.Text = "Soma5";
+            this.btnSoma5.UseVisualStyleBackColor = true;
+            this.btnSoma5.Click += new System.EventHandler(this.btnSoma5_Click);
+            // 
+            // btnSoma6
+            // 
+            this.btnSoma6.Location = new System.Drawing.Point(413, 249);
+            this.btnSoma6.Name = "btnSoma6";
+            this.btnSoma6.Size = new System.Drawing.Size(75, 23);
+            this.btnSoma6.TabIndex = 5;
+            this.btnSoma6.Text = "Soma6";
+            this.btnSoma6.UseVisualStyleBackColor = true;
+            this.btnSoma6.Click += new System.EventHandler(this.btnSoma6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSoma6);
+            this.Controls.Add(this.btnSoma5);
+            this.Controls.Add(this.btnSoma4);
+            this.Controls.Add(this.btnSoma3);
+            this.Controls.Add(this.btnSoma2);
+            this.Controls.Add(this.btnSoma1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSoma1;
+        private System.Windows.Forms.Button btnSoma2;
+        private System.Windows.Forms.Button btnSoma3;
+        private System.Windows.Forms.Button btnSoma4;
+        private System.Windows.Forms.Button btnSoma5;
+        private System.Windows.Forms.Button btnSoma6;
+    }
+}
+
---
+++ PFuncoes/PFuncoes/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PFuncoes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void Soma1()
+        {
+            Double resultado = 2 + 3;
+            MessageBox.Show("O resultado é: " + resultado);
+        }
+
+
+
+        private void btnSoma1_Click(object sender, EventArgs e)
+        {
+            Soma1();
+        }
+
+
+        Double Soma2()
+        {
+            Double resultado = 2 + 6;
+            return resultado;
+
+        }
+
+        private void btnSoma2_Click(object sender, EventArgs e)
+        {
+            double auxiliar = Soma2();
+            MessageBox.Show(auxiliar.ToString());
+            //MessageBox.Show(Soma2().ToString());
+        }
+
+        void Soma3 (double x, double y)
+        {
+            Double resultado = x + y;
+
+            MessageBox.Show("O resultado é: " + resultado.ToString());
+        }
+
+        private void btnSoma3_Click(object sender, EventArgs e)
+        {
+            Soma3(2,3);
+        }
+
+        Double Soma4(double x, double y) {
+            Double resultado = x + y;
+                return resultado;
+        }
+
+        private void btnSoma4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Soma4(2,3).ToString());
+        }
+
+        void Soma5(ref double resultado, ref double a)
+        {
+            a = 40;
+            resultado = 2 + 3;
+        }
+
+        private void btnSoma5_Click(object sender, EventArgs e)
+        {
+            double r = 0;
+            Double x = 20;
+            Soma5(ref r,ref x);
+            MessageBox.Show(x.ToString());
+            MessageBox.Show(r.ToString());
+        }
+
+        Double Soma6(ref double dobro, double x, double y)
+        {
+            dobro = (x + y) * 2;
+            return x + y;
+        }
+
+        private void btnSoma6_Click(object sender, EventArgs e)
+        {
+            double x = 2;
+            double y = 5;
+            double dobro = 0;
+            MessageBox.Show($"soma={Soma6(ref dobro, x,y)}" + $"dobro={dobro}");
+        }
+    }
+}
